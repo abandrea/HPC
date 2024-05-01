de82c2743237484f51c3fff547aabbd7d97d5a78
Andrea <andrea.buscema@outlook.it>
2024-05-01 21:59:57 +0200

    updt performance test

--- exercise_1/project_1/SBATCH/performance_test.sh
+++ exercise_1/project_1/SBATCH/performance_test.sh
@@ -6,14 +6,11 @@
 #SBATCH --partition=THIN
 #SBATCH --exclusive
 
-# Load Python module
-module load Python/3.8.2
-
-# Activate the virtual environment
-source myenv/bin/activate
+# activate the virtual environment
+source ../../../../venv/bin/activate
 
 # Run the Python script
-python my_performance_test.py
+python performance_test.py
 
 # Deactivate the environment
 deactivate
