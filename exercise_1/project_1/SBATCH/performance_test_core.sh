df65358acd07a16674ef8aca932d35e7b90b00bf
Andrea <andrea.buscema@outlook.it>
2024-05-03 19:26:15 +0200

    updt project

---
+++ exercise_1/project_1/SBATCH/performance_test_core.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=PerformanceModelCore
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-core=1
+#SBATCH --time=01:00:00
+#SBATCH --partition=THIN
+#SBATCH --exclusive
+
+module load openMPI/4.1.5/gnu/12.2.1
+
+# Define output directory and file
+output_dir="../performance_test_core"
+mkdir -p $output_dir
+output_file="${output_dir}/performance_core.csv"
+
+# Header for csv file
+echo "Size, Latency" > $output_file
+
+# Run osu_latency and capture its output
+result=$(mpirun -np 2 --map-by core ../../../../osu-micro-benchmarks-7.3/c/mpi/pt2pt/standard/osu_latency)
+
+# Process and write results to csv
+echo "$result" | grep -v '#' | grep -v 'MPI' | awk '{print $1 "," $2}' >> $output_file
+
+echo "Performance test completed"
--- exercise_1/project_1/SBATCH/performance_test_socket.sh
+++ exercise_1/project_1/SBATCH/performance_test_socket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=PerformanceModel
+#SBATCH --job-name=PerformanceModelSocket
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-socket=12
 #SBATCH --time=01:00:00
 #SBATCH --partition=THIN
 #SBATCH --exclusive
@@ -9,15 +9,15 @@
 module load openMPI/4.1.5/gnu/12.2.1
 
 # Define output directory and file
-output_dir="../performance_test3"
+output_dir="../performance_test_socket"
 mkdir -p $output_dir
-output_file="${output_dir}/performance_3.csv"
+output_file="${output_dir}/performance_socket.csv"
 
 # Header for csv file
 echo "Size, Latency" > $output_file
 
 # Run osu_latency and capture its output
-result=$(mpirun -np 2 --map-by core ../../../../osu-micro-benchmarks-7.3/c/mpi/pt2pt/standard/osu_latency)
+result=$(mpirun -np 2 --map-by socket ../../../../osu-micro-benchmarks-7.3/c/mpi/pt2pt/standard/osu_latency)
 
 # Process and write results to csv
 echo "$result" | grep -v '#' | grep -v 'MPI' | awk '{print $1 "," $2}' >> $output_file
